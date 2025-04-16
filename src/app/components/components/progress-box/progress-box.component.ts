0010935c7661d3be118381a6a9dbfffd274c24b5
Sajal Shrivastav <ss.official2018@gmail.com>
2025-04-16 21:11:08 +0530

    feature/16-Apr : component introduction

--- src/app/components/components/progress-box/progress-box.component.ts
+++ src/app/components/components/progress-box/progress-box.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-progress-box',
   imports: [],
+  standalone: true,
   templateUrl: './progress-box.component.html',
   styleUrl: './progress-box.component.css'
 })
